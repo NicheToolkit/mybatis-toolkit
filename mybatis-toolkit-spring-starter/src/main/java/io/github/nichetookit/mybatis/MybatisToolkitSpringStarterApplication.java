0aa3590ecf99486215caf13875f41a0dea0c1478
Cyan <snow22314@outlook.com>
2022-09-28 14:13:56 +0800

    v1.0.1 init

---
+++ mybatis-toolkit-spring-starter/src/main/java/io/github/nichetookit/mybatis/MybatisToolkitSpringStarterApplication.java
@@ -0,0 +1,13 @@
+package io.github.nichetookit.mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisToolkitSpringStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisToolkitSpringStarterApplication.class, args);
+    }
+
+}
---
+++ mybatis-toolkit-starter/src/main/java/io/github/nichetoolkit/mybatis/MybatisToolkitStarterApplication.java
@@ -0,0 +1,13 @@
+package io.github.nichetoolkit.mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisToolkitStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisToolkitStarterApplication.class, args);
+    }
+
+}
