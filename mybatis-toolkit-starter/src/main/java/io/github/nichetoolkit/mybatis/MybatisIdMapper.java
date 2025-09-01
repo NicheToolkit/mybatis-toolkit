71f930963e1f529525948400130ff9637bc7d21e
Cyan <snow22314@outlook.com>
2025-09-01 11:48:10 +0800

    fix

---
+++ mybatis-toolkit-starter/src/main/java/io/github/nichetoolkit/mybatis/MybatisIdMapper.java
@@ -0,0 +1,17 @@
+package io.github.nichetoolkit.mybatis;
+
+import io.github.nichetoolkit.rice.RestId;
+
+/**
+ * <code>MybatisIdMapper</code>
+ * <p>The mybatis id mapper interface.</p>
+ * @param <E> {@link io.github.nichetoolkit.rice.RestId} <p>The generic parameter is <code>RestId</code> type.</p>
+ * @param <I> {@link java.lang.Object} <p>The parameter can be of any type.</p>
+ * @author Cyan (snow22314@outlook.com)
+ * @see io.github.nichetoolkit.rice.RestId
+ * @see io.github.nichetoolkit.mybatis.MybatisSuperMapper
+ * @since Jdk1.8
+ */
+public interface MybatisIdMapper<E extends RestId<I>, I> extends MybatisSuperMapper<E, I> {
+}
+
